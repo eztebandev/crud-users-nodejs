7951fb83ca0c9d6d4fd5eff9ccad2f1328e1221d
Luis Esteban Pedraza Contreras <zzestebancontreraszz@gmail.com>
2025-06-15 23:14:24 -0500

    update db railway mysql

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;;
-const connectDB = require('./lib/connection');
+const connectDB = require('./lib/connection-railwaydb');
 
 (async () => {
 
